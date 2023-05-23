e7f60a86fdecebc7445b9b8c5d3ceda91af9325c
Vharkus <shinji_nakano036@hotmail.com>
2023-05-22 21:35:53 -0300

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double[] medidasX = LerMedidasTriangulo("X");
+            double[] medidasY = LerMedidasTriangulo("Y");
+
+            double areaX = CalcularAreaTriangulo(medidasX);
+            double areaY = CalcularAreaTriangulo(medidasY);
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
+
+        public static double[] LerMedidasTriangulo(string nomeTriangulo)
+        {
+            double[] medidas = new double[3];
+
+            Console.WriteLine("Entre com as  medidas do Triângulo " + nomeTriangulo + ":");
+            for (int i = 0; i < 3; i++)
+            {
+                medidas[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            return medidas;
+        }
+
+        public static double CalcularAreaTriangulo(double[] medidas)
+        {
+            double a = medidas[0];
+            double b = medidas[1];
+            double c = medidas[2];
+
+            double p = (a + b + c) / 2.0;
+            double area = Math.Sqrt(p * (p - a) * (p - b) * (p * c));
+
+            return area;
+        }
+    }
+}
