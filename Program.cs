0a578a50287e87bb29dcbfc3f6cdcd2cbd257e6e
Vharkus <shinji_nakano036@hotmail.com>
2023-05-22 21:51:29 -0300

    update

--- Program.cs
+++ Program.cs
@@ -1,31 +1,35 @@
-﻿using System;
+﻿//Bibliotecas na versão de C#. São bem mais práticas por conter uma enorme variedade de bibliotecas se compsarada com o C++, porém são mais lentas.
+using System;
 using System.Globalization;
 
 namespace Course
-{
+{//Classe pública que ocupa o programa todo.
     public class Program
     {
+        //Corpo principal do código.
         public static void Main(string[] args)
-        {
+        {   //Uso de Variáveis e Array para definir X e Y.
             double[] medidasX = LerMedidasTriangulo("X");
             double[] medidasY = LerMedidasTriangulo("Y");
-
+            //Variáveis para o calculo futuro das áreas.
             double areaX = CalcularAreaTriangulo(medidasX);
             double areaY = CalcularAreaTriangulo(medidasY);
-
+            //Interfaces interativas para o usuário poder escolher os números do triângulo X e Y.
             Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
             Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
-
+            //Condicional simples que mostra qual triângulo é maior, X ou Y.
             if (areaX > areaY)
+                //Interface padrão mostrada caso X seja maior que Y.
             {
                 Console.WriteLine("Maior área: X");
             }
+            //Caso seja ao contrário essa interface será mostrada.
             else
             {
                 Console.WriteLine("Maior área: Y");
             }
         }
-
+        //
         public static double[] LerMedidasTriangulo(string nomeTriangulo)
         {
             double[] medidas = new double[3];
